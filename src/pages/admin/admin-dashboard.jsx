059530e383a96e68db226d04b4fa10631b80a965
Danodya Ariyasinghe <danodyaa@boardpac.local>
2024-03-22 18:25:15 +0530

    Admin dashboard done

--- src/pages/admin/admin-dashboard.jsx
+++ src/pages/admin/admin-dashboard.jsx
@@ -2,6 +2,7 @@ import { LineChart } from '@mui/x-charts/LineChart';
 import dayjs from "dayjs";
 import customParseFormat from "dayjs/plugin/customParseFormat";
 import {BarChart} from "@mui/x-charts";
+import {Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper} from "@mui/material";
 dayjs.extend(customParseFormat);
 
 const thisWeek = [4000, 3000, 2000, 2780, 1890, 2390, 3490];
@@ -21,13 +22,50 @@ function getWeekdays() {
     return weekdays;
 }
 
+
+function createData(id, name, quantity, price) {
+    return { id, name, quantity, price  };
+}
+
 const xLabels = getWeekdays();
+
+const rows = [
+    createData('P001', 'Affairs of Hearts', 1, 2000.00),
+    createData('P002', 'Affairs of Hearts', 1, 2000.00),
+    createData('P003', 'Affairs of Hearts', 1, 2000.00),
+    createData('P004', 'Affairs of Hearts', 1, 2000.00),
+    createData('P005', 'Affairs of Hearts', 1, 2000.00),
+  ];
 
 function AdminDashboard() {
     return (
         <div className={'flex flex-col gap-5 w-full max-2xl:gap-2'}>
             <div className={'flex w-full gap-5 max-2xl:gap-2'}>
-                <div className={'w-[50%] h-[43vh] shadow-lg max-2xl:h-[42vh]'}></div>
+                <div className={'w-[50%] h-[43vh] shadow-lg max-2xl:h-[42vh] p-2 flex flex-col gap-2 items-center justify-center'}>
+                    <div className={'flex gap-2 w-full justify-between'}>
+                        <div className={'w-[50%] text-center py-2 border-2 border-secondary2 rounded-md h-[19vh] flex flex-col justify-center'}>
+                            <h1 className={'text-2xl font-semibold text-center max-2xl:text-sm'}>Total Income: This Week</h1>
+                            <div className={'text-center text-2xl font-semibold'}>Rs. 100,000.00</div>
+                        </div>
+                        
+                        <div className={'w-[50%] text-center py-2 border-2 border-secondary2 rounded-md h-[19vh] flex flex-col justify-center'}>
+                            <h1 className={'text-2xl font-semibold text-center max-2xl:text-sm'}># Orders Received: Today</h1>
+                            <div className={'text-center text-2xl font-semibold'}>15</div>
+                        </div>
+                    </div>
+                    <div className={'flex gap-2 w-full justify-between'}>
+                        <div className={'w-[50%] text-center py-2 border-2 border-secondary2 rounded-md h-[19vh] flex flex-col justify-center'}>
+                            <h1 className={'text-2xl font-semibold text-center max-2xl:text-sm'}># Pending Orders</h1>
+                            <div className={'text-center text-2xl font-semibold'}>23</div>
+                        </div>
+                        
+                        <div className={'w-[50%] text-center py-2 border-2 border-secondary2 rounded-md h-[19vh] flex flex-col justify-center'}>
+                            <h1 className={'text-2xl font-semibold text-center max-2xl:text-sm'}># Orders Delivered: Last Day</h1>
+                            <div className={'text-center text-2xl font-semibold'}>43</div>
+                        </div>
+                    </div>
+
+                </div>
                 <div className={'w-[50%] h-[43vh] shadow-lg p-5 space-y-2 flex flex-col items-center justify-center max-2xl:h-[42vh]'}>
                     <div className={'text-center'}>
                         <h1 className={'text-2xl font-semibold max-2xl:text-sm'}>Comparison of Daily Income: This Week vs. Last Week</h1>
@@ -68,7 +106,40 @@ function AdminDashboard() {
                         />
                     </div>
                 </div>
-                <div className={'w-[50%] h-[43vh] shadow-lg max-2xl:h-[42vh]'}></div>
+                <div className={'w-[50%] h-[43vh] shadow-lg space-y-2 flex flex-col items-center justify-center max-2xl:h-[42vh]'}>
+                    <div className={'text-center'}>
+                        <h1 className={'text-2xl font-semibold max-2xl:text-sm'}>Insufficient Stock Report: Low Quantity Products</h1>
+                    </div>
+                    <div className={'w-[90%] h-[35vh] flex justify-center max-2xl:w-full'}>
+                        <TableContainer >
+                            <Table stickyHeader aria-label="sticky table">
+                                <TableHead>
+                                <TableRow>
+                                    <TableCell>ID</TableCell>
+                                    <TableCell align="right">Name</TableCell>
+                                    <TableCell align="right">Quantity</TableCell>
+                                    <TableCell align="right">Price (Rs)</TableCell>
+                                </TableRow>
+                                </TableHead>
+                                <TableBody>
+                                {rows.map((row) => (
+                                    <TableRow
+                                    key={row.id}
+                                    sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                                    >
+                                    <TableCell component="th" scope="row">
+                                        {row.id}
+                                    </TableCell>
+                                    <TableCell align="right">{row.name}</TableCell>
+                                    <TableCell align="right">{row.quantity}</TableCell>
+                                    <TableCell align="right">{row.price}</TableCell>
+                                    </TableRow>
+                                ))}
+                                </TableBody>
+                            </Table>
+                        </TableContainer>
+                    </div>
+                </div>
             </div>
         </div>
     );
